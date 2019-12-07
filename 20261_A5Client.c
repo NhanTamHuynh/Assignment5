d62105685d140a047ec88d62c932f84561c8ae30
Tam Huynh <huynhnt90@gmail.com>
2019-12-07 13:51:09 -0500

    added func to read/return buffer to server

--- 20261_A5Client.c
+++ 20261_A5Client.c
@@ -5,4 +5,5 @@
 //  Created by Xcode User on 2019-12-07.
 //
 
-#include <stdio.h>
+
+
--- 20261_A5Server.c
+++ 20261_A5Server.c
@@ -6,3 +6,45 @@
 //
 
 #include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+//constants
+#define MAX 80 //message length
+#define PORT 8080
+#define SA struct sockaddr
+
+void func(int sockfd)
+{
+    char buffer[MAX];
+    int n;
+    // chat loop
+    for (;;) {
+        bzero(buff, MAX);
+
+        read(sockfd, buffer, sizeof(buff));
+                
+                //reads string for to compare for exit from client message
+                if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Left - Server Now Exiting...\n");
+            break;
+        }
+        // print which contains the client message
+        printf("Here is the message: %sEnter the string: ", buffer);
+        bzero(buffer, MAX);
+                n = 0;
+        // copy server message in the buffer
+        while ((buffer[n++] = getchar()) != '\n')
+            ;
+
+        // return buffer to client
+        write(sockfd, buffer, sizeof(buffer));
+    }
+}
+
+
